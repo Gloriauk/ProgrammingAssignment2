8bfcb4e59eaa70f25d69d4ac35141fda4d0e4f30
Gloriauk <mikedan@126.com>
2015-09-26 16:13:47 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,12 @@
-# x and m are two variables.
-# x is an invertible matrix which is need to be operated.
-# m is the stored matrix.
+## The first function, makeVector creates a special "vector", 
+  # which is really a list containing a function to
+  # 1.set the value of the vector
+  # 2.get the value of the vector
+  # 3.set the value of the mean
+  # 4.get the value of the mean.
+## x and m are two variables.
+## x is an invertible matrix which is need to be operated.
+## m is the stored matrix.
 makeCacheMatrix <- function(x = matrix()) {
         m<-NULL
         set <- function(y) {
@@ -12,13 +18,13 @@ makeCacheMatrix <- function(x = matrix()) {
         getinverse <- function() m
         list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-#x is a makeCacheMatrix.
+## x is a makeCacheMatrix.
 cacheSolve <- function(x, ...) {
-# Return a matrix that is the inverse of 'x'
-# x$getinverse is for checking if the inverse has already been calculated. 
-# If so, it gets the inverse from the cache and skips the computation. 
-# Otherwise, it calculates the inverse of the matrix x and sets the value of
-# the inverse matrix in the cache via the setinverse function.
+## Return a matrix that is the inverse of 'x'.
+## x$getinverse is for checking if the inverse has already been calculated. 
+  # If so, it gets the inverse from the cache and skips the computation. 
+  # Otherwise, it calculates the inverse of the matrix x and sets the value of
+  # the inverse matrix in the cache via the setinverse function.
         m <- x$getinverse()
         if(!is.null(m)){
                 message("getting cached data")
